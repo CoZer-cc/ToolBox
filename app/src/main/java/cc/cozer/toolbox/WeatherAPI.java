996626fdf06b5038ec92bf3bb69d2595d68d6daf
CoZer <sonehyun@live.cn>
2016-10-18 15:30:25 +0800

    ToolBox Ver1.1

--- app/src/main/java/cc/cozer/toolbox/WeatherAPI.java
+++ app/src/main/java/cc/cozer/toolbox/WeatherAPI.java
@@ -1,13 +1,21 @@
 package cc.cozer.toolbox;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.aigestudio.wheelpicker.WheelPicker;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,11 +24,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import java.io.UnsupportedEncodingException;
 ;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,23 +37,35 @@ import java.util.Map;
  * Created by soneh on 2016/10/11.
  */
 
-public class WeatherAPI extends AppCompatActivity {
+public class WeatherAPI extends AppCompatActivity implements WheelPicker.OnItemSelectedListener{
     private EditText editText;
     private Button button;
     private TextView textView;
     public String cityname;
-
-
+    List<String> citynamepicker = new ArrayList<>( );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final RequestQueue mQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.weather_data);
+        citynamepicker.add("襄阳");
+        citynamepicker.add("西安");
+        citynamepicker.add("广州");
+        citynamepicker.add("上海");
+        citynamepicker.add("北京");
+        citynamepicker.add("洛阳");
+        citynamepicker.add("郑州");
+
+       WheelPicker wheelPicker = (WheelPicker) findViewById(R.id.citypicker);
+        wheelPicker.setData(citynamepicker);
+        wheelPicker.setOnItemSelectedListener(this);
+
 
         editText = (EditText) findViewById(R.id.city_name);
         button = (Button) findViewById(R.id.getweather);
         textView = (TextView) findViewById((R.id.result));
+
         button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -54,6 +74,12 @@ public class WeatherAPI extends AppCompatActivity {
                         mQueue.add(stringRequest);
                 }
             });
+    }
+
+    @Override
+    public void onItemSelected(WheelPicker picker, Object data, int position) {
+        editText.setText(String.valueOf(data));
+
     }
 
 
